6280ef572f4591e8453efedb3862845a551d2033
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2025-01-18 18:37:39 -0500

    name change

--- src/app/feed.xml/route.js
+++ src/app/feed.xml/route.js
@@ -10,8 +10,8 @@ export async function GET(req) {
   }
 
   let author = {
-    name: 'Spencer Sharp',
-    email: 'spencer@planetaria.tech',
+    name: 'Claudia Vanea',
+    email: 'Claudia@planetaria.tech',
   }
 
   let feed = new Feed({
