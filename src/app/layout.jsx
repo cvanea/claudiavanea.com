987b8fef09214c002e9b6c8a44c7c389aaa6910a
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2025-02-04 15:10:48 -0500

    small update for search preview

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,7 +7,7 @@ export const metadata = {
   title: {
     template: '%s - Claudia Vanea',
     default:
-      'Claudia Vanea - Software designer, founder, and amateur astronaut',
+      'Claudia Vanea - Foundational and Applied AI, Oxford PhD in AI for Health',
   },
   description:
     'I’m Claudia, an AI researcher based in New York City. I recently completed a PhD at Oxford in AI for Health and I am curently exploring what’s next at South Park Commons.',
